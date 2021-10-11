c15ad5ef064d0559cd0412fad016841a0439c28b
Chris Paul <cpaul0013@gmail.com>
2021-10-11 15:03:11 -0400

    Circle Lab

---
+++ Circle Lab/Circle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle_Lab
+{
+    class Circle
+    {
+        private double Radius;
+        public double Area;
+        public double Circum;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+
+        }
+
+        public double CalculateCircumference()
+        {
+            Circum = Radius * 2 * Math.PI;
+            return Circum;
+
+           
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(Circum);
+
+
+        }
+
+        public double CalculateArea()
+        {
+            Area = Radius * Radius * Math.PI;
+            return Area;
+
+
+        }
+
+        public string CalculateFormattedArea()
+        {
+
+            return FormatNumber(Area);
+            
+
+        }
+
+        private string FormatNumber(double x)
+        {
+            return x.ToString("0.00");
+            
+
+        }
+
+
+
+
+    }
+}
---
+++ Circle Lab/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Circle_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Calculator");
+            int i = 0;
+            while(true)
+            {
+                double circleRadius = ValidRadius();
+                Circle myCircle = new Circle(circleRadius);
+
+                myCircle.Circum = myCircle.CalculateCircumference();
+                Console.WriteLine(myCircle.CalculateFormattedCircumference());
+                
+                myCircle.Area = myCircle.CalculateArea();
+                Console.WriteLine(myCircle.CalculateFormattedArea());
+
+                i++;
+
+
+            if(!KeepGoin())
+                {
+
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Good bye! You created {i} Circle objects");
+        }
+
+
+
+        public static double ValidRadius()
+        {
+            
+            double result = 0;
+            while (true)
+            {
+                Console.WriteLine("Please enter a radius");
+                string s = Console.ReadLine();
+                bool isValid = double.TryParse(s, out result);
+
+                if (isValid == true)
+                {
+                    
+                    break;
+
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+
+                }
+
+            }
+
+            return result;
+
+
+        }
+
+
+
+
+
+
+
+
+
+        public static bool KeepGoin()
+        {
+            bool keepGoing = true;
+            bool validInput = false;
+            while(validInput == false)
+            {   
+                Console.WriteLine("Would you like to enter another radius? Y / N");
+                string response = Console.ReadLine().ToLower();
+                if(response == "y")
+                {
+
+                    keepGoing = true;
+                    validInput = true;
+                }
+
+                else if(response == "n")
+                {
+                    
+                   keepGoing = false;
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid input. Please enter Y or N");
+                }
+
+            }
+            return keepGoing;
+
+
+
+        }
+    }
+}
